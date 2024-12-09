bb0f6c4795249ae3b2eea164cb72774725c41edc
Jobaer <jobaerahamed342@gmail.com>
2024-12-09 13:14:22 +0600

    my changes

--- script.js
+++ script.js
@@ -39,15 +39,15 @@ function soundAnimatio(key){
       break;
 
     case "j":
-      new Audio("sounds/tom-5.mp3").play();
+      new Audio("sounds/crash.mp3").play();
       break;
 
     case "k":
-      new Audio("sounds/tom-6.mp3").play();
+      new Audio("sounds/kick-bass.mp3").play();
       break;
 
     case "l":
-      new Audio("sounds/tom-7.mp3").play();
+      new Audio("sounds/snare.mp3").play();
       break;
       
 
